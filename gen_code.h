e264456aea41c0843c11fe3b7bf775ec93a74952
lewsive <phasing.script@gmail.com>
2024-11-20 17:44:35 -0500

    header edit

--- gen_code.h
+++ gen_code.h
@@ -1,5 +1,12 @@
-#include "gen_code.c"
-#include "bof.h"
+#ifndef GEN_CODE_H
+#define GEN_CODE_H
 
-BOFHeader gen_code_program_header(code_seq main_cs);
+#include "bof.h"
+#include "code_seq.h"
+
+// Function prototypes for the gen_code module
+void gen_code_initialize();
 void gen_code_output_seq(BOFFILE bf, code_seq cs);
+BOFHeader gen_code_program_header(code_seq main_cs);
+
+#endif // GEN_CODE_H
