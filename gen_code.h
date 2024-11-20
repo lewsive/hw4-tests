81d841851d438de5fdb63bb1621afd9dac066e09
lewsive <phasing.script@gmail.com>
2024-11-20 17:45:26 -0500

    extrrn added

--- gen_code.h
+++ gen_code.h
@@ -5,8 +5,8 @@
 #include "code_seq.h"
 
 // Function prototypes for the gen_code module
-void gen_code_initialize();
-void gen_code_output_seq(BOFFILE bf, code_seq cs);
+extern void gen_code_initialize();
+extern void gen_code_output_seq(BOFFILE bf, code_seq cs);
 BOFHeader gen_code_program_header(code_seq main_cs);
 
 #endif // GEN_CODE_H
